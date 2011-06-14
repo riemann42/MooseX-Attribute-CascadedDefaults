610d530c2fcc524f376b8322e650e473440fa7de
Edward Allen <ealleniii@cpan.org>
2011-06-14 14:09:04 -0700

    Initial go

--- lib/MooseX/Attribute/FlexibleDefaults.pm
+++ lib/MooseX/Attribute/FlexibleDefaults.pm
@@ -1,7 +1,26 @@
-use strict;
-use warnings;
 package MooseX::Attribute::FlexibleDefaults;
+use 5.008;
 
+use Moose::Exporter 1.9900;
+
+use MooseX::Attribute::FlexibleDefaults::Traits::Attribute;
+use MooseX::Attribute::FlexibleDefaults::Traits::Class;
+
+Moose::Exporter->setup_import_methods(
+    with_meta => [ 'attr_default'],
+    class_metaroles => {
+        class => ['MooseX::Attribute::FlexibleDefaults::Traits::Class'],
+        attribute => ['MooseX::Attribute::FlexibleDefaults::Traits::Attribute'],
+    },
+    role_metaroles => {
+        role => ['MooseX::Attribute::FlexibleDefaults::Traits::Class'],
+        attribute => ['MooseX::Attribute::FlexibleDefaults::Traits::Attribute'],
+    },
+);
+
+sub attr_default {
+    shift->add_builder(@_);
+}
 
 1;
 
